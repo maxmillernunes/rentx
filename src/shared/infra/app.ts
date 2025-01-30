a10601eb8a61b26915d5309ad44c3e493eb27479
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-01-29 21:36:29 -0300

    feat: :sparkles: static files

--- src/shared/infra/app.ts
+++ src/shared/infra/app.ts
@@ -8,12 +8,12 @@ import swaggerFile from '../../swagger.json';
 
 import '@shared/container';
 
+import upload from '@config/upload';
+
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
 
 import createConnection from './typeorm';
-
-import upload from '@config/upload';
 
 createConnection();
 
@@ -21,7 +21,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use('/avatar', express.static(`${upload.folder}`));
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
