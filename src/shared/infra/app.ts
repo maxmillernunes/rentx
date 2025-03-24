f4a4f2369a5a63916ee3e14dc930d2f3cdc26f2f
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-03-24 00:21:12 -0300

    fix: :pencil2: fix babel and fix the folders on the appjs

--- src/shared/infra/app.ts
+++ src/shared/infra/app.ts
@@ -21,8 +21,11 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
-app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+app.use(
+  '/avatar',
+  express.static(`${upload.tempFolder}/${upload.avatarFolder}`)
+);
+app.use('/cars', express.static(`${upload.tempFolder}/${upload.carsFolder}`));
 
 app.use(router);
 
