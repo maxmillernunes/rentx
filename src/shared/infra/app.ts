eae1db9f0fd8a52a8b1fee0d6362cdbdfed1a4d3
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-04-04 01:07:35 -0300

    ci: :green_heart: swagger ui types ignore

--- src/shared/infra/app.ts
+++ src/shared/infra/app.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express, { RequestHandler } from 'express';
+import express from 'express';
 import 'express-async-errors';
 
 import swaggerUi from 'swagger-ui-express';
@@ -21,9 +21,8 @@ createConnection();
 const app = express();
 app.use(express.json());
 
-const swaggerUiServer = swaggerUi.serve as unknown as RequestHandler;
-const swaggerUiSetup = swaggerUi.setup(swaggerFile) as RequestHandler;
-app.use('/api-docs', swaggerUiServer, swaggerUiSetup);
+// @ts-ignore
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(
   '/avatar',
